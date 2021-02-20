e0792598adad993eb3152995a2750f827266e023
irenenyatichi <irenenyatichik@gmail.com>
2021-02-20 10:13:50 +0300

    kotlintrial1

---
+++ src/hello 1.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val data= arrayOf("ireneNyatichi",20,5.5,true)
+    print(data)
+}
---
+++ src/hello.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name=("abby ")
+    print(name)
+
+    var age= 16
+    age=17
+    print(age)
+}
+
+
